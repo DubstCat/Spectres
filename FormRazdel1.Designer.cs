7c03788109710de8ad6f654b1caad363f6e08901
DubstCat <voronets.yar@gmail.com>
2023-03-24 11:29:05 +0300

    Refactored razdel1 form

--- FormRazdel1.Designer.cs
+++ FormRazdel1.Designer.cs
@@ -34,8 +34,22 @@ namespace Spectres
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chartU1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartE1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartO1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.chartU1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartE1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartO1)).BeginInit();
             this.SuspendLayout();
             // 
             // chartU1
@@ -47,33 +61,91 @@ namespace Spectres
             this.chartU1.Location = new System.Drawing.Point(12, 12);
             this.chartU1.Name = "chartU1";
             series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series2.Legend = "Legend1";
             series2.Name = "Series2";
             series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series3.Legend = "Legend1";
             series3.Name = "Series3";
             this.chartU1.Series.Add(series1);
             this.chartU1.Series.Add(series2);
             this.chartU1.Series.Add(series3);
-            this.chartU1.Size = new System.Drawing.Size(776, 362);
+            this.chartU1.Size = new System.Drawing.Size(807, 362);
             this.chartU1.TabIndex = 0;
             this.chartU1.Text = "chart1";
+            // 
+            // chartE1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chartE1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartE1.Legends.Add(legend2);
+            this.chartE1.Location = new System.Drawing.Point(825, 12);
+            this.chartE1.Name = "chartE1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Legend = "Legend1";
+            series5.Name = "Series2";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Legend = "Legend1";
+            series6.Name = "Series3";
+            this.chartE1.Series.Add(series4);
+            this.chartE1.Series.Add(series5);
+            this.chartE1.Series.Add(series6);
+            this.chartE1.Size = new System.Drawing.Size(776, 362);
+            this.chartE1.TabIndex = 1;
+            this.chartE1.Text = "chart1";
+            // 
+            // chartO1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chartO1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chartO1.Legends.Add(legend3);
+            this.chartO1.Location = new System.Drawing.Point(12, 391);
+            this.chartO1.Name = "chartO1";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series8.Legend = "Legend1";
+            series8.Name = "Series2";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series9.Legend = "Legend1";
+            series9.Name = "Series3";
+            this.chartO1.Series.Add(series7);
+            this.chartO1.Series.Add(series8);
+            this.chartO1.Series.Add(series9);
+            this.chartO1.Size = new System.Drawing.Size(776, 362);
+            this.chartO1.TabIndex = 2;
+            this.chartO1.Text = "chart2";
             // 
             // FormRazdel1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1754, 803);
+            this.Controls.Add(this.chartO1);
+            this.Controls.Add(this.chartE1);
             this.Controls.Add(this.chartU1);
             this.Name = "FormRazdel1";
             this.Text = "FormRazdel1";
             ((System.ComponentModel.ISupportInitialize)(this.chartU1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartE1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartO1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -81,5 +153,7 @@ namespace Spectres
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chartU1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartE1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartO1;
     }
 }
--- FormRazdel1.cs
+++ FormRazdel1.cs
@@ -21,18 +21,26 @@ namespace Spectres
             this.mathCad = mathCad;
             InitializeComponent();
 
+            drawChart(this.chartU1, mathCad.razdel1U1, M);
+            drawChart(this.chartE1, mathCad.razdel1E1, M);
+            drawChart(this.chartO1, mathCad.razdel1O1, M);
+
+        }
+
+        private void drawChart(System.Windows.Forms.DataVisualization.Charting.Chart chart, Func<int, List<FunctionPoint>> method, int[] M)
+        {
             int index = 0;
-            foreach (int currentM in M)
+
+            foreach(int currentM in M)
             {
-                List<FunctionPoint> grafik = mathCad.razdel1E1(currentM);
+                List<FunctionPoint> grafik = method.Invoke(currentM);
 
                 foreach (FunctionPoint point in grafik)
                 {
-                    this.chartU1.Series[index].Points.AddXY(point.y, point.x);
+                    chart.Series[index].Points.AddXY(point.y, point.x);
                 }
                 index++;
             }
-
         }
     }
 }
