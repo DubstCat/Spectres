04f42bcd7af2e6da81ad462dd1ae3c378b31a71e
DubstCat <voronets.yar@gmail.com>
2023-03-24 11:45:43 +0300

    Removed charts from initial form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,56 +29,14 @@ namespace Spectres
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.btnRazdel1 = new System.Windows.Forms.Button();
             this.btnRazdel2 = new System.Windows.Forms.Button();
-            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.btnTest = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // chart1
-            // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(34, 12);
-            this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series1.Legend = "Legend1";
-            series1.MarkerSize = 10;
-            series1.Name = "U1(t, 0)";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series2.Legend = "Legend1";
-            series2.Name = "U1(t,1)";
-            series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series3.Legend = "Legend1";
-            series3.Name = "U1(t,2)";
-            this.chart1.Series.Add(series1);
-            this.chart1.Series.Add(series2);
-            this.chart1.Series.Add(series3);
-            this.chart1.Size = new System.Drawing.Size(780, 419);
-            this.chart1.TabIndex = 0;
-            this.chart1.Text = "chart1";
             // 
             // btnRazdel1
             // 
-            this.btnRazdel1.Location = new System.Drawing.Point(334, 437);
+            this.btnRazdel1.Location = new System.Drawing.Point(52, 224);
             this.btnRazdel1.Name = "btnRazdel1";
             this.btnRazdel1.Size = new System.Drawing.Size(92, 23);
             this.btnRazdel1.TabIndex = 1;
@@ -88,7 +46,7 @@ namespace Spectres
             // 
             // btnRazdel2
             // 
-            this.btnRazdel2.Location = new System.Drawing.Point(1166, 437);
+            this.btnRazdel2.Location = new System.Drawing.Point(615, 224);
             this.btnRazdel2.Name = "btnRazdel2";
             this.btnRazdel2.Size = new System.Drawing.Size(81, 23);
             this.btnRazdel2.TabIndex = 2;
@@ -96,37 +54,9 @@ namespace Spectres
             this.btnRazdel2.UseVisualStyleBackColor = true;
             this.btnRazdel2.Click += new System.EventHandler(this.btnRazdel2_Click);
             // 
-            // chart2
-            // 
-            chartArea2.Name = "ChartArea1";
-            this.chart2.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart2.Legends.Add(legend2);
-            this.chart2.Location = new System.Drawing.Point(820, 12);
-            this.chart2.Name = "chart2";
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series4.Legend = "Legend1";
-            series4.MarkerSize = 10;
-            series4.Name = "E1(t, 0)";
-            series5.ChartArea = "ChartArea1";
-            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series5.Legend = "Legend1";
-            series5.Name = "E1(t,1)";
-            series6.ChartArea = "ChartArea1";
-            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series6.Legend = "Legend1";
-            series6.Name = "E1(t,2)";
-            this.chart2.Series.Add(series4);
-            this.chart2.Series.Add(series5);
-            this.chart2.Series.Add(series6);
-            this.chart2.Size = new System.Drawing.Size(780, 419);
-            this.chart2.TabIndex = 3;
-            this.chart2.Text = "chart2";
-            // 
             // btnTest
             // 
-            this.btnTest.Location = new System.Drawing.Point(703, 448);
+            this.btnTest.Location = new System.Drawing.Point(331, 224);
             this.btnTest.Name = "btnTest";
             this.btnTest.Size = new System.Drawing.Size(92, 23);
             this.btnTest.TabIndex = 4;
@@ -140,24 +70,17 @@ namespace Spectres
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1639, 498);
             this.Controls.Add(this.btnTest);
-            this.Controls.Add(this.chart2);
             this.Controls.Add(this.btnRazdel2);
             this.Controls.Add(this.btnRazdel1);
-            this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Button btnRazdel1;
         private System.Windows.Forms.Button btnRazdel2;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
         private System.Windows.Forms.Button btnTest;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -23,51 +23,19 @@ namespace Spectres
 
         private void btnRazdel1_Click(object sender, EventArgs e)
         {
-            this.chart1.Series[0].Points.Clear();
-            this.chart1.Update();
-            int index = 0;
-            foreach(int currentM in M)
-            {
-                List<FunctionPoint> grafik = mathCad.razdel1U1(currentM);
-
-                foreach (FunctionPoint point in grafik)
-                {
-                    this.chart1.Series[index].Points.AddXY(point.y, point.x);
-                }
-                index++;
-            }
+            var formRazdel1 = new FormRazdel1(mathCad, M);
+            formRazdel1.Activate();
+            formRazdel1.Show();
         }
 
         private void btnRazdel2_Click(object sender, EventArgs e)
         {
-            this.chart2.Series[0].Points.Clear();
-            this.chart2.Update();
             
-            int index = 0;
-            foreach (int currentM in M)
-            {
-                List<FunctionPoint> grafik = mathCad.razdel1E1(currentM);
-
-                foreach (FunctionPoint point in grafik)
-                {
-                    this.chart2.Series[index].Points.AddXY(point.y, point.x);
-                }
-                index++;
-            }
         }
 
         private void btnTest_Click(object sender, EventArgs e)
         {
-            var formRazdel1 = new FormRazdel1(mathCad, M);
-            formRazdel1.Activate();
-            formRazdel1.Show();
             
-            List<FunctionPoint> grafik = mathCad.razdel2U2(6);
-
-            foreach(FunctionPoint point in grafik)
-            {
-                this.chart2.Series[0].Points.AddXY(point.y, point.x);
-            }
         }
     }
 }
